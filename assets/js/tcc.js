060d67206109ae6e8707ba44239f63928eb16a75
Igor <iandrosov73@gmail.com>
2023-03-07 22:43:16 +0900

    WIP add language switch

--- assets/js/tcc.js
+++ assets/js/tcc.js
@@ -1,3 +1,15 @@
-$(function(){
-    $('.selectpicker').selectpicker();
-});
+
+//$(function(){
+//    $('.selectpicker').selectpicker();
+//});
+const switchLanguage = function(lang) {
+    alert(lang);
+    window.location.href = lang;
+    window.location = lang;
+}
+$(".nav").change(function() {
+    window.location.href = $(".nav").value;
+    window.location = $(".nav").val;
+    alert($(".nav").val());
+  });
+  
